09c70f90e112909e6a49fae17711ddc16b27e30b
Ali Nurdin <alinurdin159@gmail.com>
2022-10-30 01:28:33 +0700

    integrasi api

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Auth with ChangeNotifier {
   var _token = null;
@@ -23,19 +24,44 @@ class Auth with ChangeNotifier {
         },
         body: jsonEncode({'nik': email, 'password': password}));
 
-    // return json.decode(response.body);
+    return jsonDecode(response.body);
+  }
+
+  Future changePassword (oldPassword, newPassword) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('TOKEN');
+
+    final response = await http.post(Uri.parse('$baseUrl/account/update-password'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'Accept': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+        body: jsonEncode({'old_password': oldPassword, 'new_password': newPassword}));
 
     return jsonDecode(response.body);
+  }
 
-    // if (response.body.status == 'success') {
-    //   this.token = response.body.token;
-    // }
+  Future changePhoto (payload) async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('TOKEN');
 
-    // print(output);
+    final url = Uri.parse('$baseUrl/account/update-photo');
+    final request = new http.MultipartRequest('POST', url);
 
-    // print(jsonEncode({
-    //   'username': email,
-    //   'password': password
-    // }));
+    request.headers.addAll({
+      'Authorization': 'Bearer $token',
+      'Content-Type': 'multipart/form-data',
+      "Keep-Alive": "timeout=5, max=1000"
+    });
+
+    final photo = await http.MultipartFile.fromPath('photo', payload['photo'].path);
+
+    request.files.add(photo);
+
+    final response = await request.send();
+    final responsed = await http.Response.fromStream(response);
+
+    return jsonDecode(responsed.body);
   }
 }
